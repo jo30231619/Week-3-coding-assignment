4c0b711db80d2ba2a8405f64375bd7940d404c63
Joseph <68339401+jo30231619@users.noreply.github.com>
2022-08-22 12:48:28 -0400

    git Init

---
+++ codingAssignment1.js
@@ -0,0 +1,6 @@
+//1.	Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+//a.	Programmatically subtract the value of the first element in the array from the value in the last element of the array 
+//      (do not use numbers to reference the last element, find it programmatically, ages[7] – ages[0] is not allowed). Print the result to the console.
+//b.	Add a new age to your array and repeat the step above to ensure it is dynamic (works for arrays of different lengths).
+//c.	Use a loop to iterate through the array and calculate the average age. Print the result to the console.
+let ages = [3, 9, 23, 64, 2, 8, 28, 93];
